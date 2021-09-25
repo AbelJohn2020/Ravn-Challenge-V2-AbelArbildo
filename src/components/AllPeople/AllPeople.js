6215ea1a1163394be9af1eacb5d5be34396a64b7
Abel AR <abel_john_14@hotmail.com>
2021-09-24 19:13:28 -0500

    Add react-icons and give styles to the options

--- src/components/AllPeople/AllPeople.js
+++ src/components/AllPeople/AllPeople.js
@@ -1,15 +1,20 @@
-import React from 'react'
+import React from 'react';
+import './AllPeople.css';
+import { AiOutlineRight } from "react-icons/ai"
 
 const AllPeople = ({getPeople}) => {
     return (
         <div>
             {
                 getPeople.map(({ id, name, species, homeworld}) => (
-                    <div className = "person" key={id}>
-                        <h1>{ name }</h1>
-                        <p>
-                            {species ? `${ species.name }` : 'Human'} from { homeworld.name }
-                        </p>
+                    <div className = "box" key={id}>
+                        <div className="box__person">
+                            <h1 className="box__person--name">{ name }</h1>
+                            <p className="box__person--planet">
+                                {species ? `${ species.name }` : 'Human'} from { homeworld.name }
+                            </p>
+                        </div>
+                        <AiOutlineRight />
                     </div>
                 ))
             }
--- src/components/GetAllPeople/GetAllPeople.js
+++ src/components/GetAllPeople/GetAllPeople.js
@@ -2,10 +2,11 @@ import React from 'react';
 import AllPeople from '../AllPeople/AllPeople';
 import ErrorMessage from '../ErrorMessage/ErrorMessage';
 import Loading from '../Loading/Loading';
+import './GetAllPeople.css';
 
 const GetAllPeople = ({loading, error, getPeople}) => {
     return (
-        <div>
+        <div className="getAllPeople">
             {
                 loading && <Loading />
             }
